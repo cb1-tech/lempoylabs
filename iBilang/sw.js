fb4f718b2f14cd73fc25e4b082e6a57901c3812e
cb1-tech <carobar.tradecars@gmail.com>
2026-09-19 00:24:13 +0900

    Add standalone PDF layouts for all reports

--- iBilang/sw.js
+++ iBilang/sw.js
@@ -1,4 +1,4 @@
-const CACHE='ibilang-v9';
+const CACHE='ibilang-v10';
 const FILES=['./','./index.html','./style.css','./app.js','./db.js','./domain.js','./manifest.webmanifest','./apple-touch-icon.png','./icon-192.png','./icon-512.png','./icon-maskable-192.png','./icon-maskable-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
